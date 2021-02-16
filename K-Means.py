7ae34e22f4f60ab27569c8c5dbcfcfc0e9dd389f
jinnykim1 <79124531+jinnykim1@users.noreply.github.com>
2021-02-16 18:01:24 +0900

    Update K-Means.py

--- K-Means.py
+++ K-Means.py
@@ -1,19 +1,29 @@
 from sklearn.cluster import KMeans
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import numpy as np
+
+#dummy data
+Z= pd.DataFrame(columns=['x','y'])
+Z.loc[0] =[2,3]
+Z.loc[1] =[2,11]
+Z.loc[2] =[3,10]
+
 #클러스터의 개수 지정(n개)
-num_clusters = 3
+num_clusters = 4
 #데이터셋 Z 삽입
 km = KMeans(n_clusters=num_clusters)
 km.fit(Z)
 
 
-import matplotlib.pyplot as plt
-import numpy as np
 
 from scipy.spatial.distance import cdist
 distortions = []
 K = range(1,10)
 for k in K:
-    kmeanModel = KMeans(n_clusters=k).fit(Z)
+    kmeanModel = KMeans(n_clusters=k, init='k-means++').fit(Z)
     kmeanModel.fit(Z)
     distortions.append(sum(np.min(cdist(Z, kmeanModel.cluster_centers_, 'euclidean'), axis=1)) / Z.shape[0])
 # Plot the elbow
